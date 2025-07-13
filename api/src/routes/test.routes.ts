bb3754e36f86b6e5288a1bbff4af72ffeff00222
Bilal Amara <amara.bilalpro@gmail.com>
2025-07-13 02:21:32 +0200

    feat: complete CRUD implementation and migrate backend from PostgreSQL to MySQL

--- api/src/routes/test.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/test', (_req, res) => {
-  res.status(200).json({ message: 'API is working ✅' });
-});
-
-export default router;
